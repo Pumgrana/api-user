9d131fbfdc996e9068011a19ddeed4459b5169e8
tristan djahel <tdjahel@laptop.home>
2017-02-02 19:41:09 +0000

    Nouvel version avec les index not_analyzed d'elasticsearch

---
+++ app/models/history.rb
@@ -0,0 +1,18 @@
+require 'elasticsearch/model'
+
+class History < ApplicationRecord
+  include Elasticsearch::Model
+
+  mapping do
+    indexes :email, index: 'not_analyzed'
+    indexes :url, index: 'not_analyzed'
+    indexes :origin_url, index: 'not_analyzed'
+    indexes :target_url, index:'not_analyzed'
+    indexes :lang, index:'not_analyzed'
+    indexes :image,index: 'not_analyzed'
+  end
+end
+
+puts History.mappings.to_hash
+History.__elasticsearch__.client = Elasticsearch::Client.new host:'172.17.0.2:9200'
+History.import force: true
