40db5fa16805d62f465fe8903ea24c07c89670c4
tristan djahel <tdjahel@laptop.home>
2017-02-02 19:38:00 +0000

    Remove all for new version. Kill them all

--- app/models/history.rb
+++
@@ -1,9 +0,0 @@
-require 'elasticsearch/model'
-
-class History < ApplicationRecord
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
-end
-
-History.__elasticsearch__.client = Elasticsearch::Client.new host: 'http://172.17.0.2:9200/'
-History.import force: true
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -d -e "9100:9100" api-user:latest
